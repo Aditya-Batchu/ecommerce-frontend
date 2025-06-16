de527a5ff429976a714b7eded83584691be2eb6a
Aditya Batchu <batchuaditya369@gmail.com>
2025-06-16 18:34:56 +0530

    created automated testing for format currency

---
+++ tests/moneyTest.js
@@ -0,0 +1,28 @@
+import {formatCurrency} from '../scripts/utils/money.js';
+
+
+console.log('test suite: formatCurrency')
+
+console.log("converts cents to dollars")
+
+if(formatCurrency(2095) === '20.95'){
+  console.log("passed");
+}else{
+  console.log('failed');
+}
+
+console.log("works with zero")
+
+if(formatCurrency(0) === '0.00'){
+  console.log("passed");
+}else{
+  console.log('failed');
+}
+
+console.log("rounding number to nearest integer")
+
+if(formatCurrency(2005.5) === '20.06'){
+  console.log("passed");
+}else{
+  console.log('failed');
+}
