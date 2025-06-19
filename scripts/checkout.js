5195ed0c6a265d444928411824048565dbfcdf38
Aditya Batchu <batchuaditya369@gmail.com>
2025-06-19 12:44:03 +0530

    Learn promises and converted code into promises

--- scripts/checkout.js
+++ scripts/checkout.js
@@ -3,9 +3,54 @@ import { renderPaymentSummary } from "./Checkout/paymentSummary.js";
 import { loadProducts } from "../data/products.js";
 // import '../data/cart-class.js';
 //import '../data/backend-practice.js';
+import { loadCart } from "../data/cart.js";
 
-loadProducts(()=>{
+Promise.all([
+  new Promise((resolve)=>{
+    loadProducts(()=>{
+      resolve('1');
+    })
+
+  }),
+  new Promise((resolve)=>{
+    loadCart(()=>{
+      resolve('2');
+    });
+  })
+
+]).then((values)=>{
+  console.log(values)
   renderOrderSummary();
   renderPaymentSummary();
-});
+})
+
+
+// new Promise((resolve)=>{
+//   loadProducts(()=>{
+//     resolve('1');
+//   })
+
+// }).then((value)=>{
+//   console.log(value)
+
+//   return new Promise((resolve)=>{
+//     loadCart(()=>{
+//       resolve('2');
+//     });
+
+//   })
+// }).then((value)=>{
+//    console.log(value)
+
+//    renderOrderSummary();
+//    renderPaymentSummary();
+// })
+
+
+// loadProducts(()=>{
+//   loadCart(()=>{
+//     renderOrderSummary();
+//     renderPaymentSummary();
+//   });
+// });
 
