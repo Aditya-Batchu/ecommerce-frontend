b866f84a4ce95d2b06452ea7a8e4f086cfc0f799
Aditya Batchu <batchuaditya369@gmail.com>
2025-06-19 13:01:13 +0530

    convert loasProducts from xmlHttp request to fetch

--- data/products.js
+++ data/products.js
@@ -86,6 +86,29 @@ console.log(date.toLocaleDateString())
 
 export let products =[];
 
+export function loadProductsFetch(){
+  const promise = fetch(
+    'https://supersimplebackend.dev/products'
+  ).then((response)=>{
+     return (response.json())
+  }).then((productsData)=>{
+    products = productsData;
+    products = products.map((productDetails)=>{
+      if(productDetails.type === "clothing"){
+        return new Clothing(productDetails);
+      }else{
+        return new Product(productDetails);
+      }
+    });
+    console.log('load products');
+  });
+  return promise;
+}
+
+// loadProductsFetch().then(()=>{
+//   console.log('next step')
+// });
+
 export function loadProducts(fun){
   
   const xhr = new XMLHttpRequest();
--- scripts/checkout.js
+++ scripts/checkout.js
@@ -1,17 +1,13 @@
 import { renderOrderSummary } from "./Checkout/orderSummary.js";
 import { renderPaymentSummary } from "./Checkout/paymentSummary.js";
-import { loadProducts } from "../data/products.js";
+import { loadProducts,loadProductsFetch } from "../data/products.js";
 // import '../data/cart-class.js';
 //import '../data/backend-practice.js';
 import { loadCart } from "../data/cart.js";
 
 Promise.all([
-  new Promise((resolve)=>{
-    loadProducts(()=>{
-      resolve('1');
-    })
-
-  }),
+  loadProductsFetch()
+  ,
   new Promise((resolve)=>{
     loadCart(()=>{
       resolve('2');
