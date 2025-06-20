a5fc7d2df039805ff66102b27acce7c2b5c7bd2c
Aditya Batchu <batchuaditya369@gmail.com>
2025-06-20 10:21:06 +0530

    create urder using backend but it does support url

--- scripts/Checkout/paymentSummary.js
+++ scripts/Checkout/paymentSummary.js
@@ -2,6 +2,7 @@ import { cart } from "../../data/cart.js";
 import { getProduct } from "../../data/products.js";
 import { getDeliveryOption } from "../../data/deliveyOptions.js";
 import { formatCurrency } from '../utils/money.js';
+import { addOrder } from "../../data/orders.js";
 
 export function renderPaymentSummary(){
 
@@ -63,11 +64,35 @@ export function renderPaymentSummary(){
     </div>
   </div>
 
-  <button class="place-order-button button-primary">
+  <button class="place-order-button button-primary 
+    js-place-order">
     Place your order
   </button>
 
   `;
 
   document.querySelector('.js-payment-summary').innerHTML = paymentSummaryHTML;
+
+  document.querySelector('.js-place-order')
+    .addEventListener('click',async()=>{
+
+      try{
+        const response = await fetch('https://supersimplebackend.dev/orders',{
+          method:'POST',
+          headers:{
+            'Content-Ttype':'application/json',
+          },
+          body:JSON.stringify({cart : cart})
+        });
+
+        const order = await response.json();
+        addOrder(order);
+      }catch(error){
+        console.log(error);
+        console.log('Unexpected error. Try again later!');
+      }
+      
+      window.location.href = 'orders.html';
+      console.log(cart)
+    })
 }
